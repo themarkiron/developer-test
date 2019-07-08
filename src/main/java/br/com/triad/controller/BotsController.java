0c88c0b195732447171c74544261706d9036186d
Felipe Rodrigues <felipe.rodrigues@keyrus.com.br>
2019-07-08 16:27:20 -0300

    implements API operation Swagger controller

--- src/main/java/br/com/triad/controller/BotsController.java
+++ src/main/java/br/com/triad/controller/BotsController.java
@@ -3,15 +3,21 @@ package br.com.triad.controller;
 import br.com.triad.dto.BotsDTO;
 import br.com.triad.service.DefaultBotsService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/bots")
+@Api(value = "Bots Controller", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 public class BotsController {
 
 
@@ -23,6 +29,11 @@ public class BotsController {
     @GetMapping(value = "/{id}")
     @ResponseBody
     @Cacheable("boots")
+    @ApiOperation("get specific bots for id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "bots received with success"),
+            @ApiResponse(code=404,message = "The id specific not exist")
+    })
     public String getBotsPerId(@PathVariable String id) throws IOException
     {
         ObjectMapper objectMapper = new ObjectMapper();
@@ -34,6 +45,7 @@ public class BotsController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Create bots into  database")
     public  void createBots(@RequestBody  BotsDTO botsDTO)
     {
         defaultBotsService.createBots(botsDTO);
@@ -42,6 +54,11 @@ public class BotsController {
 
     @PutMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("update specific bots per id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200,message = "bots update with success"),
+            @ApiResponse(code=404,message = "The id specific not exist to be update")
+    })
     public void updateBots(@RequestBody BotsDTO botsDTO) throws  Exception
     {
         defaultBotsService.updateBots(botsDTO.getId(),botsDTO.getName());
@@ -49,6 +66,7 @@ public class BotsController {
     }
 
     @DeleteMapping(value = "/{id}")
+    @ApiOperation("delete bots into  database per id")
     @ResponseStatus(HttpStatus.OK)
     public void deleteBotsForId(@PathVariable String id){
 
